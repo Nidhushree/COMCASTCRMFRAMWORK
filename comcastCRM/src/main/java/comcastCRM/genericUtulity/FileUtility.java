e252ba443882114eacf7b00b129f4d6b248055b7
Admin <Admin@DESKTOP-S9COC0S.mshome.net>
2024-12-14 11:28:37 +0530

    COMCAST REPO

---
+++ comcastCRM/src/main/java/comcastCRM/genericUtulity/FileUtility.java
@@ -0,0 +1,18 @@
+package comcastCRM.genericUtulity;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class FileUtility {
+	public String getDataFromPropertiesFile(String key) throws IOException
+	{
+		FileInputStream fis=new FileInputStream("./conFigAppData/commondata1.property");
+		Properties pObj=new Properties();
+		pObj.load(fis);
+		String data = pObj.getProperty(key);
+		System.out.println("hi");
+		return data;
+	}
+
+}
---
+++ comcastCRM/src/main/java/comcastCRM/genericUtulity/JasonUtility.java
@@ -0,0 +1,15 @@
+package comcastCRM.genericUtulity;
+
+import java.io.IOException;
+
+
+public class JasonUtility {
+	public String getDataFromJsonFile( String key) 
+	
+	{
+		System.out.println("Hi");
+		return key;
+	}
+
+}
+
